f1b721e9896cb0e5428374fd01f82140ca7d01b8
Kbrow555 <kbrow555@students.kennesaw.edu>
2026-07-13 21:27:23 -0400

    Add header and navigation includes to index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include 'includes/header.php'; ?>
+<?php include 'includes/nav.php'; ?>
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

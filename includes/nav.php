a9a76d8dad6cdefa2467682193e13f7e7cbcf087
Kbrow555 <kbrow555@students.kennesaw.edu>
2026-07-13 22:22:26 -0400

    Delete includes/nav.php

--- includes/nav.php
+++
@@ -1,8 +0,0 @@
-<nav>
-    <a href="index.php">Home</a>
-    <a href="facilities.php">Facilities & Labs</a>
-    <a href="research.php">Research & Opportunities</a>
-    <a href="eligibility.php" class="active">Eligibility Evaluator</a>
-    <a href="application.php">Application</a>
-</nav>
-</header>

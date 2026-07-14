be38ddb961430ca530a156d7fbab12efe74e6666
Kbrow555 <kbrow555@students.kennesaw.edu>
2026-07-13 20:51:55 -0400

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facilities & Labs</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <a href="https://www.kennesaw.edu/ccse/" target="_blank">
+       <img src="./img/ksu-logo.png" alt="Kennesaw State University College of Computing and Software Engineering Logo" class="logo">
+    </a>
+
+    <h1>Robin and Doug Shore Innovation Center</h1>
---
+++ includes/nav.php
@@ -0,0 +1,8 @@
+nav>
+    <a href="index.php">Home</a>
+    <a href="facilities.php">Facilities & Labs</a>
+    <a href="research.php">Research & Opportunities</a>
+    <a href="eligibility.php" class="active">Eligibility Evaluator</a>
+    <a href="application.php">Application</a>
+</nav>
+</header>
